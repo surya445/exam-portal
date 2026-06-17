21f4c7b1eccaf0a0cc6576a2b014f64905b70091
surya445 <suryaprakashsharma422@gmail.com>
2026-06-17 19:24:50 +0530

    Fix homepage

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
+const path = require("path");
 const express = require("express");
 const fs = require("fs");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 const DB = "database.json";
 
