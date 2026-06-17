bb1c4d15353bcb3f1ecf914e1adcb96504916c7a
surya445 <suryaprakashsharma422@gmail.com>
2026-06-17 19:11:22 +0530

    Render port update

--- server.js
+++ server.js
@@ -574,6 +574,8 @@ app.delete("/admin/delete-exam/:id", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
